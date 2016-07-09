b1c941502885eb83ee7e1cc08458f34f10ce5181
920624269 <920624269@qq.com>
2016-07-10 01:27:29 +0800

    kkkkk

---
+++ www/js/ffs1.js
@@ -0,0 +1,9 @@
+// angular.module('ffs.controller', [])
+//   .controller('httpCtrl', function ($scope, $http) {
+//   $http.get('/www/mock/ffs.json')
+//     .then(
+//       function (res) {
+//         $scope.livelist = res.data.data;
+//       }
+//     )
+// })
